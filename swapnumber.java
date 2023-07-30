9f8e5dab9478d6149914dea3376b7c9c70719432
abhishekdasarp <139537140+abhishekdasarp@users.noreply.github.com>
2023-07-30 19:59:30 +0530

    Add files via upload

---
+++ swapnumber.java
@@ -0,0 +1,7 @@
+class swapnumber {
+    public static void main(string args[]) {
+        int a = 10, b = 20;
+        system.out.println("a: " + a);
+        system.out.println("b:" + b);
+    }
+}
